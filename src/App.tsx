5cae42c2afb32c195267ec33984241868140459c
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-18 11:40:58 -0500

    navigation bar function and position, intro/header effect, hamburger menu function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import Intro from './components/intro/Intro';
 import Header from './components/header/Header';
 import About from './components/about/About';
 import './App.scss';
 
 function App() {
+  const [navOpen, setNavOpen] = useState<Boolean>(false);
   return (
-    <body>
+    // react fragment allows us to group our sections under one parent (as per React) w/o creating a new div
+    // (good practice to limit extraneous divs)
+    <Fragment>
       <Intro />
-      <Header />
-      <About />
-    </body>
+      <Header navOpen={navOpen} setNavOpen={setNavOpen} />
+      <main>
+        <About />
+      </main>
+    </Fragment>
   );
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,19 @@
 import './header.scss';
 import Trees from '../../images/trees.png'
+import Navigation from './nav/Navigation';
 
-export default function Header() {
+export default function Header(props: any) {
+    const { navOpen, setNavOpen } = props;
+
     return (
         <header className='header'>
-            <img src={Trees} className='trees' alt='a dark blue treeline'/>
+        <img src={Trees} className='trees' alt='a dark blue treeline'/>
+            <div onClick={() => setNavOpen(!navOpen)} className={`hamburger ${navOpen ? 'active' : ''}`}>
+                <span className='line1'></span>
+                <span className='line2'></span>
+                <span className='line3'></span>
+            </div>
+        <Navigation navOpen={navOpen}/>
         </header>
     )
 }
