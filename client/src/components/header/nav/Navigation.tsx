855f586280e04cceec7c82d8d8ff256ff126d1dd
Nick Zabkar <nzabkar51@gmail.com>
2021-11-20 13:37:56 -0500

    Changed nav to work with react components

--- client/src/components/header/nav/Navigation.tsx
+++ client/src/components/header/nav/Navigation.tsx
@@ -3,13 +3,19 @@ import './navigation.scss';
 export default function Navigation(props:any)   {
     const { navOpen } = props;
 
-    return  (
+    return (
         <nav className={`Nav ${navOpen ? '' : 'hidden'}`}>
             <ul className='nav-list'>
                 {/* TODO: list functionality */}
-                <li><a href='#'>Log In</a></li>
-                <li><a href='#'>Blog</a></li>
-                <li><a href='#'>Unknown</a></li>
+                <li id="small-text" className="navbtn btn col 4 light-blue darken-3" onClick={() => props.handlePageChange('AboutMe')}>
+                    Home
+                </li>
+                <li id="small-text" className="navbtn btn col 4 light-blue darken-3" onClick={() => props.handlePageChange('Portfolio')}>
+                    Dashboard
+                </li>
+                <li id="small-text" className="navbtn btn col 4 light-blue darken-3" onClick={() => props.handlePageChange('Contact')}>
+                    Unknown
+                </li>
             </ul>
         </nav>
     )
