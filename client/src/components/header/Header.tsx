3af2b9050f7f3507afb88595df49932489b50a65
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-20 13:53:18 -0500

    restructure header, add searchbar

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,5 +1,7 @@
 import './header.scss';
-import Trees from '../../images/trees.png'
+import Trees from '../../images/trees.png';
+import Searchbar from './search/Searchbar';
+import Hamburger from './hamburger/Hamburger';
 import Navigation from './nav/Navigation';
 
 export default function Header(props: any) {
@@ -7,13 +9,12 @@ export default function Header(props: any) {
 
     return (
         <header className='header'>
-        <img src={Trees} className='trees' alt='a dark blue treeline'/>
-            <div onClick={() => setNavOpen(!navOpen)} className={`hamburger ${navOpen ? 'active' : ''}`}>
-                <span className='line1'></span>
-                <span className='line2'></span>
-                <span className='line3'></span>
-            </div>
-        <Navigation navOpen={navOpen}/>
+            <img src={Trees} className='trees' alt='a dark blue treeline'/>
+            <section className='top'>
+                <Searchbar />
+                <Hamburger navOpen={navOpen} setNavOpen={setNavOpen}/>
+            </section>
+            <Navigation navOpen={navOpen}/>
         </header>
     )
 }
---
+++ client/src/components/header/search/Searchbar.tsx
@@ -0,0 +1,9 @@
+import './search.scss';
+
+export default function Searchbar() {
+    return  (
+        <div>
+            <input name='search' className='search' placeholder='🔍'></input>
+        </div>
+    )
+}
