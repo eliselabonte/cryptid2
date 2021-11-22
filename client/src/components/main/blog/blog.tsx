3d714c9b753541c3483d45afab8c4bb664149dab
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-22 10:04:05 -0500

    minor media queries, remove Category folder, add blog display, add useState for display (not functioning yet), title card position move based on display setting, nav open wrap around searchbar

---
+++ client/src/components/main/blog/blog.tsx
@@ -0,0 +1,74 @@
+import './blog.scss';
+
+export default function Blog() {
+    return  (
+        <div className='blog'>
+            <h2 className='blog-title'>UFO Reports</h2>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
