52c72a773a8453fcac8b264608c7d0d028f7ae73
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-23 14:53:26 -0500

    structure and styling of profile elements, add seed script to server, adjust display of auth cards, react conditional for tagline, adjust treeline display, add variable for white cards

--- client/src/components/main/auth/Authentication.tsx
+++ client/src/components/main/auth/Authentication.tsx
@@ -8,14 +8,17 @@ export default function Authentication () {
                 <form id='login' className='auth-form'>
                     <input id='email-login' placeholder='email'></input>
                     <input id='password-login' placeholder='password'></input>
+                    <button id='login-button' className='auth-button'>Log In</button>
                 </form>
             </div>
+            <h2>OR</h2>
             <div className='card signup'>
                 <h4>Sign Up</h4>
                 <form id='signup' className='auth-form'>
                     <input id='email-signup' placeholder='email'></input>
                     <input id='username-signup' placeholder='username'></input>
                     <input id='password-signup' placeholder='password'></input>
+                    <button id='signup-button' className='auth-button'>Create Account</button>
                 </form>
             </div>
         </div>
