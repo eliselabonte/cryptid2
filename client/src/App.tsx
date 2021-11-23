9e62c5ccaafdd0d71c5d6c6acc19a78fc491fd61
Stevanie Kanter <stevaniek@yahoo.com>
2021-11-22 20:47:51 -0500

    added auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
-import React, {Fragment, useState} from 'react';
+import React, { Fragment, useState } from 'react';
 import Intro from './components/intro/Intro';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import './App.scss';
+import Auth0ProviderWithHistory from './auth0Provider';
 
 function App() {
   const [navOpen, setNavOpen] = useState<Boolean>(false);
 
   const [display, setDisplay] = useState('Homepage');
   return (
+    <Auth0ProviderWithHistory>
     // react fragment allows us to group our sections under one parent (as per React) w/o creating a new div
-    // (good practice to limit extraneous divs)
-    <Fragment>
-      <Intro />
-      <Header 
-      navOpen={navOpen} 
-      setNavOpen={setNavOpen} 
-      display={display}
-      setDisplay={setDisplay}/>
-      <Main display={display}/>
-    </Fragment>
+      // (good practice to limit extraneous divs)
+      <Fragment>
+        <Intro />
+        <Header
+          navOpen={navOpen}
+          setNavOpen={setNavOpen}
+          display={display}
+          setDisplay={setDisplay} />
+        <Main display={display} />
+      </Fragment>
+    </Auth0ProviderWithHistory>
   );
 }
 
