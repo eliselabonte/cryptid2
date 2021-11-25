d8503b2252aa0c5fb32bfd24f6342499c95cb227
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-24 20:00:14 -0500

    add dashboard, search landing, model relationship for creature table

--- client/src/components/main/dashboard/Dashboard.tsx
+++ client/src/components/main/dashboard/Dashboard.tsx
@@ -4,7 +4,47 @@ export default function Dashboard()  {
     return  (
         <div className='dashboard'>
             <h2>DASHBOARD</h2>
-            <p>Contains posts of users and tags that the logged-in user follows</p>
+            <h3 className='dashboard-description'>Posts that \username\ has saved</h3>
+
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
+            <div className='report'>
+                <section className='report-info'>
+                    <h3 className='report-title'>Report Title</h3>
+                    <h4 className='report-name-date'>by Name on Date</h4>
+                </section>
+                <p className='report'>Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report Report report report report.</p>
+                <ul className='tags-list'>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                    <li className='report-tag'>#tag</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- client/src/components/main/searchLanding/SearchLanding.tsx
+++ client/src/components/main/searchLanding/SearchLanding.tsx
@@ -2,6 +2,8 @@ import './searchLanding.scss';
 
 export default function SearchLanding() {
     return (
+        // renders other users profiles
+        // access posts that match a tag
         <div></div>
     )
 }
