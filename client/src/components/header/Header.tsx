c40d7528bb3a3297be41f9aeb990f948f4a100ca
Stevanie Kanter <stevaniek@yahoo.com>
2021-11-29 21:48:33 -0500

    configure auth0

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -4,13 +4,13 @@ import Searchbar from './search/Searchbar';
 import Hamburger from './hamburger/Hamburger';
 import Navigation from './nav/Navigation';
 // added auth 0
-import { useAuth0 } from "@auth0/auth0-react";
+import AuthNav from './login/auth-nav';
 
 
 export default function Header(props: any) {
     const { navOpen, setNavOpen, display, setDisplay } = props;
 
-    const { loginWithRedirect, logout, user, isLoading } = useAuth0();
+    // const { loginWithRedirect, logout, user, isLoading } = useAuth0();
 
     return (
         <header className='header'>
@@ -28,22 +28,7 @@ export default function Header(props: any) {
                     {/* <a href='#scrollDown' 
                     className='login-button' 
                     onClick={() => setDisplay('Authentication')}>Log In</a> */}
-                    
-                    {/* auth 0 button */}
-                    {!isLoading && !user && (
-                    <button
-                        className={ navOpen ? 'login-button open' : 'login-button'}
-                        onClick={() => loginWithRedirect()}> 
-                        Log In
-                    </button>
-                    )}
-                    {!isLoading && user && (
-                    <button
-                        className={ navOpen ? 'login-button open' : 'login-button'}
-                        onClick={() => logout()}> 
-                        Log Out
-                    </button>
-                    )}
+                    <AuthNav/>
                 </div>
             </section>
             <Navigation 
