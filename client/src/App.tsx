062b074ce79e6c94b9a27d06db10f0e4ffa80abb
Nick Zabkar <nzabkar51@gmail.com>
2021-12-02 14:16:25 -0500

    Added route for create page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,32 +7,12 @@ import Dashboard from "./components/switchDisplay/dashboard/Dashboard";
 import Profile from "./components/switchDisplay/profile/Profile";
 import SearchLanding from "./components/switchDisplay/searchLanding/SearchLanding";
 import useSearch from './useSearch';
-<<<<<<< HEAD
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
-import Create from "./components/switchDisplay/create/create";
-=======
 import { read } from "fs";
->>>>>>> 18336c417d47832860e9881bbed3045d168ea84c
+import Create from "./components/switchDisplay/create/create";
 
 export default function AllRoutes(children: any) {
     return (
         <BrowserRouter>
-<<<<<<< HEAD
-            <Auth0ProviderWithHistory>
-                <Routes>
-                    <Route path="/" element={<Layout search={search} setSearch={setSearch} />}>
-                        <Route path='/' element={<Homepage />} />
-                        <Route path='/blog/:id' element={<Blog />} />
-                        <Route path='/creatures/:id' element={<Creatures />} />
-                        <Route path='/dashboard' element={<Dashboard />} />
-                        <Route path='/profile/:id' element={<Profile />} />
-                        <Route path='/create' element={<Create />} />
-                        <Route path='/search/:searchItems'
-                            element={<SearchLanding results={results} />} />
-                    </Route>
-                </Routes>
-            </Auth0ProviderWithHistory>
-=======
             <Routes>
                 <Route path="/" element={<Layout search={"search"} setSearch={() => { }} />}>
                     <Route path='/' element={<Homepage />} />
@@ -40,11 +20,11 @@ export default function AllRoutes(children: any) {
                     <Route path='/creatures/:id' element={<Creatures />} />
                     <Route path='/dashboard' element={<Dashboard />} />
                     <Route path='/profile/:id' element={<Profile />} />
+                    <Route path='/create' element={<Create />} />
                     <Route path='/search/:searchItems'
                         element={<SearchLanding results={"results"} />} />
                 </Route>
             </Routes>
->>>>>>> 18336c417d47832860e9881bbed3045d168ea84c
         </BrowserRouter>
     )
 }
