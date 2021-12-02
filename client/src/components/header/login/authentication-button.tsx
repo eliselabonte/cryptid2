eae10b3a47c4df9315a17e9b1425a9077cfae1ab
Nick Zabkar <nzabkar51@gmail.com>
2021-12-02 14:23:02 -0500

    Redo commits

--- client/src/components/header/login/authentication-button.tsx
+++ client/src/components/header/login/authentication-button.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import LoginButton from './login-button';
 import LogoutButton from './logout-button';
-
 import { useAuth0 } from '@auth0/auth0-react';
 
 const AuthenticationButton = () => {
   const { isAuthenticated } = useAuth0();
-  console.log(isAuthenticated)
+  useEffect(() => {
+    console.log(isAuthenticated)
+  }, [isAuthenticated])
+
 
   return isAuthenticated ? <LogoutButton /> : <LoginButton />;
 };
