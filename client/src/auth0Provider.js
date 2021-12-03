4870de4ee54608b3199a3d02ec54363043884015
Nick Zabkar <nzabkar51@gmail.com>
2021-12-02 20:15:46 -0500

    Fixed Auth0?

--- client/src/auth0Provider.js
+++ client/src/auth0Provider.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-export const Auth0Provider = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+export const Auth0Handler = ({ children }) => {
+
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT;
+  const domain = "dev-wfgeclbx.us.auth0.com";
 
   return (
     <Auth0Provider
