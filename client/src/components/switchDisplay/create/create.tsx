ea5f67f3e1c6a5f8987f85062823ec9c1bf24142
Elise_LaBonte <emlabonte714@gmail.com>
2021-12-04 08:49:19 -0500

    style form

--- client/src/components/switchDisplay/create/create.tsx
+++ client/src/components/switchDisplay/create/create.tsx
@@ -1,28 +1,30 @@
+import './create.scss'
+
 export default function Create() {
     return (
         
-        <div>
-            <h3>Report a sighting</h3>
-            <p>Be sure to use our search feature to see if others have had a similar experience</p>
+        <div className='create-div'>
+            <h3 className='report-div-title'>Report a sighting</h3>
+            <p className='search-info'>Be sure to use our search feature to see if others have had a similar experience</p>
             <form className="form new-sighting-form" id="sighting">
                 <div className="form-group">
-                    <label htmlFor="sighting-report">Report</label>
+                    <label htmlFor="sighting-report" className='label'>Report </label>
                     <textarea className="form-input" id="sighting-report" name="sighting-report" required></textarea>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="sighting-description">Description of cryptid</label>
+                    <label htmlFor="sighting-description" className='label'>Description of creature </label>
                     <textarea className="form-input" id="sighting-description" name="sighting-description" required></textarea>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="sighting-location">Location of sighting</label>
+                    <label htmlFor="sighting-location" className='label'>Location of sighting </label>
                     <input className="form-input" type="text" id="sighting-location" name="sighting-name" required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="sighting-time">Time Seen</label>
-                    <textarea className="p-3 form-input" id="sighting-time" name="sighting-time" placeholder="year-month-date hour:minute"required></textarea>
+                    <label htmlFor="sighting-time" className='label'>Time Seen </label>
+                    <textarea className="p-3 form-input" id="sighting-time" name="sighting-time" placeholder="yyyy/mm/dd h:m"required></textarea>
                 </div>
                 <div>
-                    <label htmlFor="category">Choose a category</label>
+                    <label htmlFor="category" className='label'>Choose a category to report this sighting under: </label>
                     <select name="category" id="category">
                         <option value="1">Cryptid</option>
                         <option value="2">UFO</option>
