672d94db0ddbc4943033e77810702a7d917a31d4
Nick Zabkar <nzabkar51@gmail.com>
2021-12-03 21:36:38 -0500

    Reverted dashboard

--- client/src/components/switchDisplay/dashboard/Dashboard.tsx
+++ client/src/components/switchDisplay/dashboard/Dashboard.tsx
@@ -8,11 +8,30 @@ export default function Dashboard(props:any) {
 
     const safePosts = results || [];
 
-    return  (
-        <div className='dashboard'>
-            <h2 className='dashboard-title'>Dashboard</h2>
-            <h3 className='dashboard-description'>All Saved Posts</h3>
-        </div>
-    )
+        return  (
+            <div className='dashboard'>
+                <h2 className='dashboard-title'>Dashboard</h2>
+                <h3 className='dashboard-description'>All Posts</h3>
+                {safePosts.map((report:any) =>{
+                    const safeTags = report.tags || []
+                    return(
+                        <div className='report' key={report.id}>
+                            <section className='report-info'>
+                                <h3 className='report-description'>{report.description}</h3>
+                                <h4 className='report-name-date'>by {report.user} location:{report.location}</h4>
+                            </section>
+                            <p className='report'>{report.report}</p>
+                            <ul className='tags-list'>
+                                {safeTags.map((tag:any) =>{
+                                    return(
+                                        <li className='report-tag' key={tag.id}>#{tag.name}</li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    )
+                })}
+            </div>
+        )
 
 }
