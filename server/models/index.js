fddfedc5c22d936aa39a252954f88914e6f0d2d6
Elise_LaBonte <emlabonte714@gmail.com>
2021-12-16 14:39:33 -0500

    time format on posts

--- server/models/index.js
+++ server/models/index.js
@@ -23,8 +23,8 @@ Post.belongsTo(Category, {
 // user has many posts which they have written,
 // user has many posts which they have favorited,
 // posts can be saved by many users
-Post.belongsToMany(User, {
-  through: UserPost
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
 User.belongsToMany(Post, {
