540261a534596d084061a573b77bc64d6852da13
Elise_LaBonte <emlabonte714@gmail.com>
2022-01-14 15:05:11 -0500

    form functions

--- client/src/components/views/create/create.tsx
+++ client/src/components/views/create/create.tsx
@@ -1,35 +1,50 @@
 import './create.scss';
 import axios from 'axios';
-import $ from 'jquery';
 import { useAuth0 } from '@auth0/auth0-react';
-import moment from 'moment';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+interface Istate {
+    report:'', 
+    description:'', 
+    locationSeen:'', 
+    timeSeen:'', 
+    category_id:number, 
+    user_id:number}
 
 export default function Create(props:any) {
+    const { register, handleSubmit } = useForm();
+
     const { user, isAuthenticated } = useAuth0();
     const { userId } = props;
     const navigate = useNavigate();
 
+    const [newReport, setNewReport] = useState<Istate>({
+            report:'', 
+            description:'', 
+            locationSeen:'', 
+            timeSeen:'', 
+            category_id:1, 
+            user_id:userId})
+
+    const updateField = (key:string) => (event:any) => { 
+        setNewReport({...newReport, [key]: event.target.value}) }
+
     function submitForm(e:any) {
         e.preventDefault()
 
+        const user_id = userId;
+        
         if(!isAuthenticated) {
             window.alert('please log in to create a report.')
         }
         else {
-            const report = $('#report').val();
-            const description = $('#description').val();
-            const location = $('#location').val();
-            const dateTime = $('#time').val();
-            const timeSeen = moment(dateTime).format('YYYY-MM-DD HH:mm') || undefined;
-            const category = $('#category');
-            const category_id = category.val();
-            const user_id = userId;
+            const {report, description, locationSeen, timeSeen, category_id, user_id} = newReport
             console.log(user_id)
             axios.post(`/api/posts/`, {
             report, 
             description, 
-            location, 
+            location: locationSeen, 
             timeSeen, 
             category_id, 
             user_id
@@ -52,7 +67,9 @@ export default function Create(props:any) {
                     <label htmlFor="category" className='label'>Choose a category to report this sighting under: </label>
                     {/* TODO: scss doesn't register this as a child without a div wrapping it */}
                     <div>
-                        <select name="category" id="category">
+                        <select name="category" id="category" 
+                        value={newReport.category_id}
+                        onChange={updateField('category_id')}>
                             <option value="1">Cryptid</option>
                             <option value="2">UFO</option>
                             <option value="3">Paranormal</option>
@@ -62,19 +79,36 @@ export default function Create(props:any) {
                 </div> 
                 <div className="form-group">
                     <label htmlFor="report" className='label'>Report</label>
-                    <textarea className="form-input report" id="report" name="report" required></textarea>
+                    <textarea 
+                    className="form-input report" 
+                    id="report" name="report" 
+                    value={newReport.report} 
+                    onChange={updateField('report')} required></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="description" className='label'>Description of creature</label>
-                    <textarea className="form-input description" id="description" name="description" required></textarea>
+                    <textarea 
+                    className="form-input description" 
+                    id="description" name="description"
+                    value={newReport.description} 
+                    onChange={updateField('description')} required></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="location" className='label'>Location</label>
-                    <textarea className="form-input" id="location" name="name" required ></textarea>
+                    <textarea 
+                    className="form-input" 
+                    id="location" name="name"
+                    value={newReport.locationSeen} 
+                    onChange={updateField('locationSeen')} required ></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="time" className='label'>Time Seen</label>
-                    <input className="form-input" id="time" name="time" type='datetime-local' required/>
+                    <input 
+                    className="form-input" 
+                    id="time" name="timeSeen" 
+                    type='datetime-local'
+                    value={newReport.timeSeen} 
+                    onChange={updateField('timeSeen')} required/>
                 </div> 
                 <div className="form-group submit-button">
                     <button type="submit">Report</button>
